514326f50a694e5312fbcedde35900d5bf6fdf75
Young Guo <young.guo@gmail.com>
2018-10-29 01:21:15 -0700

    Create basic frontend and integrate backend

--- part3/app.py
+++ part3/app.py
@@ -1,40 +1,38 @@
 import os
-from flask import Flask, render_template, g
+from flask import Flask, render_template, g, request
 import requests
+import random
 
 app = Flask(__name__, instance_relative_config=True)
 
 @app.route('/')
 def index(name=None):
-    print("in index")
-    g.name = name
     return render_template('index.html')
 
 @app.route('/categories')
 def categories():
     categories = requests.get('http://jservice.io/api/categories/', data={'count': 10})
     categories_json = categories.json()
-    ids = [category['id'] for category in categories_json]
-    titles = [category['title'] for category in categories_json]
-    print(ids, titles, categories_json)
-    return render_template('categories.html')
+    return render_template('categories.html', **locals())
 
 @app.route('/category/<category_id>')
 def get_question_from_category(category_id=None):
-    return render_template('question.html')
+    question_list = requests.get('http://jservice.io/api/category?id=' + category_id).json()['clues']
+    question_obj = random.choice(question_list)
+    question, answer = question_obj['question'], question_obj['answer']
+    return render_template('question.html', **locals())
 
 @app.route('/question')
 def get_random_question():
     question_obj = requests.get('http://jservice.io/api/random').json()[0]
     question, answer = question_obj['question'], question_obj['answer']
-    print(question_obj)
-    print(question, answer)
-    return render_template('question.html')
+    return render_template('question.html', **locals())
 
-@app.route('/answer')
+@app.route('/answer', methods=['POST'])
 def check_answer():
-    print('check answer')
-    return render_template('answer.html')
+    input_answer = request.form['input_answer']
+    real_answer = request.form['real_answer']
+    return render_template('answer.html', **locals())
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
