98f665def47b823408aa60cc52ca27d58a54b695
Young Guo <young.guo@gmail.com>
2018-10-29 00:46:02 -0700

    Add debug flag to run function

--- part3/app.py
+++ part3/app.py
@@ -38,5 +38,5 @@ def check_answer():
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
-    # aapp.run(host='0.0.0.0', port=port)
-    app.run()
+    # app.run(host='0.0.0.0', port=port, debug=True)
+    app.run(debug=True)
