139005733c963df6cb5481f52dac648e2bbf6387
Amitav Baruah <abaruah@berkeley.edu>
2018-10-31 02:17:28 -0700

    Remove use of **locals

--- app.py
+++ app.py
@@ -13,27 +13,27 @@ def index(name=None):
 def categories():
     categories = requests.get('http://jservice.io/api/categories/', data={'count': 6})
     categories_json = categories.json()
-    return render_template('categories.html', **locals())
+    return render_template('categories.html', categories_json=categories_json)
 
 @app.route('/category/<category_id>')
 def get_question_from_category(category_id=None):
     question_list = requests.get('http://jservice.io/api/category', data={'id': category_id}).json()['clues']
     question_obj = random.choice(question_list)
     question, answer = question_obj['question'], question_obj['answer']
-    return render_template('question.html', **locals())
+    return render_template('question.html', question=question, answer=answer)
 
 @app.route('/question')
 def get_random_question():
     question_obj = requests.get('http://jservice.io/api/random').json()[0]
     question, answer = question_obj['question'], question_obj['answer']
-    return render_template('question.html', **locals())
+    return render_template('question.html', question=question, answer=answer)
 
 @app.route('/answer', methods=['GET'])
 def check_answer():
     input_answer = request.args['input_answer']
     real_answer = request.args['real_answer']
     correct_answer_string = 'Correct' if input_answer.lower() == real_answer.lower() else 'Incorrect'
-    return render_template('answer.html', **locals())
+    return render_template('answer.html', input_answer=input_answer, real_answer=real_answer, correct_answer_string=correct_answer_string)
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
